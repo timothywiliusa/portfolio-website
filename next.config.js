29b42a953a8266ea8f8bc3df8328ab8153911cd6
timothywiliusa <43688005+timothywiliusa@users.noreply.github.com>
2024-04-18 15:40:18 -0700

    Revert next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/porfolio-website"
-    output: "export",
-    reactStrictMode: true,
     images: {
         remotePatterns: [
             {
